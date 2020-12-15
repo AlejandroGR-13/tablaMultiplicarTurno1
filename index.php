b7eabd2f72de8cb999c75b3308eb3ed74efd4612
AlejandroGR-13 <guerra.alejandro1999@gmail.com>
2020-12-15 08:16:40 +0000

    Anado la tabla del 3ñ

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 for($i=1; $i<=10; $i++){
-	$resultado=2;
-	$resultado*=$i;	
+	$resultado=2*$i;	
 	echo("2 por $i = $resultado<br>");
+}
+
+for($i=1; $i<=10; $i++){
+	$resultado=3*$i;
+	echo("3 por $i= $resultado<br>");
 }
 ?>
